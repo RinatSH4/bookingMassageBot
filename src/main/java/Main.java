22168746b3ff5df2189fc98f7b0eca4635fa19cd
MacBook Pro <macbookpro@RinatSH.local>
2023-11-25 12:51:59 +0500

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,6 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         try {
@@ -12,6 +10,7 @@ public class Main {
 
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
+
         }
 
     }
